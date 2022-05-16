9473394d520118c2a924d0eaf1488b2107142525
kiminpyo <93189402+kiminpyo@users.noreply.github.com>
2022-05-17 01:38:52 +0900

    Create NumberofK.js

---
+++ Lv.1/NumberofK.js
@@ -0,0 +1,23 @@
+
+
+const array = [1, 5, 2, 6, 3, 7, 4]
+
+function solution(array, commands) {
+    
+    var answer = [];
+    return answer;
+}
+console.log(array.slice(i-1,j).sort())
+const a = array.slice(i-1,j).sort();
+console.log(a[k])
+
+function commands(i,j,k){
+        let arr =[];
+        for(let b=0 ; b< array.length; b++){
+            const sort = array[b].slice(i,j).sort();
+            const result = sort[k];
+            result.push(arr)
+        }
+      
+                    }
+                   console.log(commands(1,2,3))
