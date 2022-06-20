16ac00bfd55af0e67902e0c26bb6514f55c967da
kiminpyo <rladlsvy2448@naver.com>
2022-06-21 02:40:08 +0900

    add numberofK

--- Lv.1/NumberofK.js
+++ Lv.1/NumberofK.js
@@ -1,23 +1,10 @@
-
-
-const array = [1, 5, 2, 6, 3, 7, 4]
-
 function solution(array, commands) {
-    
-    const answer = [];
-    return answer;
-}
-console.log(array.slice(i-1,j).sort())
-const a = array.slice(i-1,j).sort();
-console.log(a[k])
-
-function commands(i,j,k){
-        let arr =[];
-        for(let b=0 ; b< array.length; b++){
-            const sort = array[b].slice(i,j).sort();
-            const result = sort[k];
-            result.push(arr)
-        }
+    const a = []
+    for(let i=0; i<commands.length;i++){
+      const b = array.slice(commands[i][0]-1,commands[i][1])
+        b.sort((a,b) => a-b) ;
+        a.push(b[commands[i][2]-1])
       
-                    }
-                   console.log(commands(1,2,3))
+} 
+    return a
+}
